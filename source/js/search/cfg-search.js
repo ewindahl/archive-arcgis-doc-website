53e7be763c78cc3785561011810170153e0094b0
Sanjib Panda <spanda@esri.com>
2013-09-04 10:47:25 -0700

    Marketplace search/feedback/404
    Fixed few issues. Also added search,feedback,404, landing page for
    marketplace.

--- source/js/search/cfg-search.js
+++ source/js/search/cfg-search.js
@@ -10,8 +10,11 @@
              emptyImgUrl: "/en/shared/rc/gallery/emptyimg.png",
              errorMsg: "Please try again.",
              nullMsg: "No results found.",
+             searchInterfaceName: "developers",
+             searchViewName: "arcgis_doc_en",
              mdfL: ["search-collection:4","search-category:4","product:7"],
              kv: {"all-collections":"All Collections","help":"Help","forums":"Forum","blogs":"Blog","video":"Video","any-apps":"Any of these ArcGIS apps","android-app":"ArcGIS app (Android)", "ios-app":"ArcGIS app (iOS)","win-phone-app":"Win Phone App","collector-android":"Collector (Android)", "collector-ios":"Collector (iPhone)", "esri-maps-office":"Maps for Office", "ops-dashboard":"Ops Dashboard"},
              contribkv : {"esri":"Esri Maps & Apps","partner":"Partner Maps & Apps"},
-             cntL: [15,30,45]
+             cntL: [15,30,45],
+             localizeSearch: "Y"
          };
