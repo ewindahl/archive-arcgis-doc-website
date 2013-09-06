4671212b6dd52d8a7a4651944d0f2df2837d4940
tchen <tchen@esri.com>
2013-09-06 09:39:54 -0700

    add marketplace js and remove dev js

--- source/js/feedback.js
+++
@@ -1,1 +0,0 @@
-require(["dojo/query","Config","dojo/domReady!","dojo/NodeList-dom"],function(e,t){e("#source").attr("value","internal"),e("#redirect-url").attr("value",window.location.protocol+"//"+window.location.host+"/en/thank-you"),e("#feedback-form").attr("action",t.feedback_url)});
