b974b13806bcb2d0194d34d3b3c165e5bf6fe5d7
tchen <tchen@esri.com>
2013-10-11 21:22:48 -0700

    build script

---
+++ pyscript/buildl10n.py
@@ -0,0 +1,100 @@
+import os
+import sys
+import traceback
+
+import shutil
+import subprocess
+
+import libl10n
+
+libl10n.langs
+
+rootDir = os.path.normpath (os.path.join (os.getcwd(), ".."))
+buildDir = os.path.normpath (os.path.join (rootDir, "..", "tmp"))
+
+
+def doBuild (lg, srcRoot, buildDir):
+   op = os.path
+
+   print "srcRoot: ", srcRoot
+   print "buildDir: ", buildDir
+
+   dstRoot = op.join (buildDir, lg)
+   print "mkdir ", dstRoot
+   os.makedirs(dstRoot)
+
+   for f in os.listdir (srcRoot):
+      srcfp = op.join (srcRoot, f)
+      if op.isfile (srcfp):
+         dstfp = op.join (dstRoot, f)
+         print "copy {0} -> {1}".format (srcfp, dstfp)
+         shutil.copy(srcfp, dstfp)
+
+   #copy locales
+   srcfp = op.join (srcRoot, "locales.other", "{0}.yml".format (lg))
+   dstd = op.join (dstRoot, "locales")
+   dstfp = op.join (dstd, "{0}.yml".format (lg))
+   os.makedirs(dstd)
+   print "copy {0} -> {1}".format (srcfp, dstfp)
+   shutil.copyfile(srcfp, dstfp)
+
+   #copy source
+   srcd = op.join (srcRoot, "source.{0}".format (lg))
+   dstd = op.join (dstRoot, "source")
+   print "copy {0} -> {1}".format (srcd, dstd)
+   shutil.copytree(srcd, dstd)
+
+   #copy source/layouts
+   srcd = op.join (srcRoot, "source/layouts")
+   dstd = op.join (dstRoot, "source/layouts")
+   print "copy {0} -> {1}".format (srcd, dstd)
+   shutil.copytree(srcd, dstd)
+
+   #copy source/localizable/_partial
+   srcd = op.join (srcRoot, "source/localizable/_partial")
+   dstd = op.join (dstRoot, "source/localizable/_partial")
+   print "copy {0} -> {1}".format (srcd, dstd)
+   shutil.copytree(srcd, dstd)
+
+   #build
+   os.chdir (dstRoot)
+   print "cd ", os.getcwd()
+   print subprocess.check_output(["bundle", "exec", "middleman", "build"])
+
+   #copy result
+   srcd = op.join (dstRoot, "build", lg)
+   dstd = op.join (srcRoot, "build", lg)
+   if os.path.exists (dstd):
+      print "rm ", dstd
+      shutil.rmtree(dstd)
+
+   print "copy {0} -> {1}".format (srcd, dstd)
+   shutil.copytree(srcd, dstd)
+
+def main():
+   print "rootDir: ", rootDir
+   print "buildDir: ", buildDir
+
+   lgL = []
+   v = sys.argv[1]
+   if  v == "all":
+      lgL = libl10n.langs
+   elif v in libl10n.langs:
+      lgL = [v]
+   else:
+      print "invalide lang ", v
+      return
+
+   if os.path.exists (buildDir):
+      shutil.rmtree(buildDir)
+   os.makedirs(buildDir)
+
+   print "build ", lgL
+   for lg in lgL:
+      try:
+         doBuild (lg, rootDir, buildDir)
+
+      except:
+         print traceback.format_exc()
+
+main()
