14ee3466145521ede96c10f855e891e5efb44709
tchen <tchen@esri.com>
2013-10-15 11:26:49 -0700

    fix missing switcher bug

--- pyscript/libl10n.py
+++ pyscript/libl10n.py
@@ -1,5 +1,5 @@
 langs = [
-"ar", "da", "de", "fr", "it", 
-"ja", "ko", "no", "pl", "pt-br", 
+"ar", "da", "de", "es", "fr", "it", 
+"ja", "js", "ko", "no", "pl", "pt-br", "pt-pt" 
 "ro", "ru", "sv", "zh-cn"
 ]
