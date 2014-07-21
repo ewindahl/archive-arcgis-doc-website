9b0cda966b33613962eed05a6c4bd8a2cc8ce593
sanjibpanda <spanda@esri.com>
2014-07-21 11:05:21 -0700

    Mapping product names with possible relevant tags (for Forums)

--- source/js/search/searchcfg.js
+++ source/js/search/searchcfg.js
@@ -101,7 +101,7 @@ var filterCfg = {
         q:{
             r:"",
             p:"product:android-app|product:ios-app|product:win-phone-app|product:collector-android|product:collector-ios|product:esri-maps-office|product:esri-maps-sharepoint|product:ops-dashboard|product:arcgis-online|product:explorer-ipad|product:explorer-iphone|product:explorer-mac|product:esri-demographics|product:arcgis-geoplanner",
-            pForums:"keywords:app|keywords:android|keywords:ios|keywords:windows phone|keywords:collector|keywords:maps for office|keywords:maps for sharepoint"
+            pForums:"keywords:app|keywords:android|keywords:ios|keywords:windows phone|keywords:collector|keywords:maps for office|keywords:maps for sharepoint|keywords:dashboard||keywords:operations dashboard|keywords:explorer|keywords:iphone|keywords:ipad|keywords:geoplanner|keywords:demographics|keywords:open-data"
           }
       },
 	  {
@@ -109,35 +109,39 @@ var filterCfg = {
         q: {
           r:"",
           p:"(product:arcgis-online)",
-          pForums:"(keywords:Arcgis Online|keywords:Online)"
+          pForums:"(keywords:Arcgis Online|keywords:Online|keywords:trust|keywords:open data)"
         }
       }, 
       {
         k:"android-app",
         q: {
           r:"",
-          p:"(product:android-app)"
+          p:"(product:android-app)",
+          pForums:"(keywords:android|keywords:ArcGIS app)"
         }
       },
       {
         k:"ios-app",
         q: {
           r:"",
-          p:"(product:ios-app)"
+          p:"(product:ios-app)",
+          pForums:"(keywords:ios|keywords:ArcGIS app)"
         }
       }, 
       {
         k:"win-phone-app",
         q: {
           r:"",
-          p:"(product:win-phone-app)"
+          p:"(product:win-phone-app)",
+          pForums:"(keywords:Windows Phone|keywords:ArcGIS app)"
         }
       },
       {
         k:"collector-android",
         q: {
           r:"",
-          p:"(product:collector-android)"
+          p:"(product:collector-android)",
+          pForums:"(keywords:android|keywords:Collector)"
         }
       },
 
@@ -145,49 +149,56 @@ var filterCfg = {
         k:"collector-ios",
         q: {
           r:"",
-          p:"(product:collector-ios)"
+          p:"(product:collector-ios)",
+          pForums:"(keywords:ios|keywords:Collector)"
         }
       },
 	  {
         k:"esri-demographics",
         q: {
           r:"",
-          p:"(product:esri-demographics)"
+          p:"(product:esri-demographics)",
+          pForums:"(keywords:demographics)"
         }
       },
 	  {
         k:"explorer-ipad",
         q: {
           r:"",
-          p:"(product:explorer-ipad)"
+          p:"(product:explorer-ipad)",
+          pForums:"(keywords:explorer|keywords:ipad)"
         }
       },
 	  {
         k:"explorer-iphone",
         q: {
           r:"",
-          p:"(product:explorer-iphone)"
+          p:"(product:explorer-iphone)",
+          pForums:"(keywords:explorer|keywords:iphone)"
         }
       },
-	  /*{
+	  {
         k:"explorer-mac",
         q: {
           r:"",
-          p:"(product:explorer-mac)"
+          p:"(product:explorer-mac)",
+          pForums:"(keywords:explorer|keywords:mac)"
         }
-      },*/
+      },
 	  {
         k:"arcgis-geoplanner",
         q: {
           r:"",
-          p:"(product:arcgis-geoplanner)"
+          p:"(product:arcgis-geoplanner)",
+          pForums:"(keywords:geoplanner)"
         }
       },
       {
         k:"esri-maps-office",
         q: {
           r:"",
-          p:"(product:esri-maps-office)"
+          p:"(product:esri-maps-office)",
+          pForums:"(keywords:maps for office|keywords:office)"
         }
       },
       {
@@ -210,7 +221,8 @@ var filterCfg = {
         q: {
           r:"",
           p:"(product:ops-dashboard)",
-          pForums:"(keywords:operations|keywords:dashboard)"
+          pForums:"(keywords:operations|keywords:dashboard)",
+          pForums:"(keywords:dashboard|keywords:operations dashboard)"
         }
       },
 
