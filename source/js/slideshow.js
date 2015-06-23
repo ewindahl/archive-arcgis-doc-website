13c5a9124f2093818ec37ea65be49b2a3a1cec2e
Bradley Wertman <bwertman@esri.com>
2015-06-23 11:08:13 -0700

    initial about livingatlas page work

---
+++ source/js/slideshow.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  console.log("ready");
+  $('.slideshow').cycle({
+    timeout: 0,
+    manualSpeed: 200,
+    slides: '> .slide',
+    pager: '> .cycle-pager'
+  });
+});
