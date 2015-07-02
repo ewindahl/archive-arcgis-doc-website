7ebd2491f303d28ad49901171a0d408c63d272b9
sanjibpanda <spanda@esri.com>
2015-07-02 14:29:52 -0700

    typo fix

--- pyscript/libl10n.py
+++ pyscript/libl10n.py
@@ -1,5 +1,5 @@
 langs = [
 "ar", "da", "cs", "de", "es", "et", "fi", "fr", "he", "it", 
 "ja", "ko", "lt", "lv", "nl", "no", "pl", "pt-br", "pt-pt", 
-"ro", "ru", "sv", "th", "tr", "zh-cn", "zh-hk", "zh-tw",
+"ro", "ru", "sv", "th", "tr", "zh-cn", "zh-hk", "zh-tw"
 ]
