d4127cb38772051c110ed2e8f4179d4a3a945e17
sanjibpanda <spanda@esri.com>
2015-09-18 15:47:34 -0700

    Fix for issue#2664

--- source/js/signIn.js
+++ source/js/signIn.js
@@ -115,7 +115,7 @@ agolLogout
 	        $("#logged-in-navigation > a").html (avatar+"<span>"+ text +"</span>");
 
           $("#agolProfile").attr ("href", "//" + orgHostname + sitecfg["agolProfile"]);
-          $("#agolHelp").attr ("href", "//" + orgHostname + sitecfg["agolHelp"]);
+          $("#agolHelp").attr ("href", sitecfg["agolHelp"]);
 		  $("#agolLogout").attr ("href", "https://" + orgHostname + sitecfg["agolSignout"]+"?redirect_uri=https:"+sitecfg["portalHostname"] + sitecfg["agolSignout"]+"?redirect_uri="+encodeURIComponent(window.location.href));
     
           $(".myconsole").css ("display", "block");
