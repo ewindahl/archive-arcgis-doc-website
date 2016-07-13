082fc36413b1397a0bf9be9a4fb21dbe003fdab8
sanjibpanda <spanda@esri.com>
2016-07-13 16:55:43 -0700

    Fix for issue# 3711 & # 3790

---
+++ source/en/asset/js/survey123-1.js
@@ -0,0 +1,147 @@
+$(document).ready(function() {
+
+/*
+	data-appname="survey123"
+		data-plat="desktop"
+		data-plat="browser"
+
+*/
+   var localedir = "en";
+   if(window.docConfig !== undefined){
+      localedir =   docConfig['localedir'];
+   }
+   var dict = (window.localeJsonObj || {})[localedir];
+   
+    var val = '<p id="plats">' +
+        '<span class="viewing" data-langlabel="viewing">' + dict['viewing'] + ': </span>' +
+		'<a data-appname="survey123" data-plat="desktop" data-prefix="/' + localedir +'/survey123/desktop" href="/en/survey123/" data-langlabel="desktop" class=""> Desktop</a>' +
+        ' | ' +
+        '<a data-appname="survey123" data-plat="browser" data-prefix="/' + localedir +'/survey123/browser" href="/en/survey123/" data-langlabel="browser" class=""> Browser</a>' +
+        '</p>',
+
+		prodKey = "survey123",
+		prodDVal = "desktop",
+		prodDesktopVal = "desktop",
+		prodBrowserVal = "browser",
+		homePath = "/en/survey123"
+    
+		pathname = window.location.pathname,
+		parts = pathname.split ("/"),
+		fname = parts.pop(),
+		fldpath = parts.join ("/"),
+		plat = $.cookie (prodKey) || prodDVal,
+		isHome = fldpath === homePath;
+
+    if(!($.cookie (prodKey)) && !(navigator.userAgent.match(/Windows NT/gi))) {
+      plat = prodBrowserVal;
+      if (!isHome && !pathname.match( /(\/browser\/)/)) {
+         UASpecificRedirect (plat, pathname);
+      }
+    }
+	 
+	function isValidPattern(pattern) {
+		if (!pattern.match( /(\/analyze-results\/)/)){
+			return true;
+		} else {
+			return false;
+		}
+	}		
+   
+	function UASpecificRedirect (plat, pathname) {
+
+			var parts = pathname.split("/");
+				fname = parts.pop(),
+				fld = parts.pop(),
+				newHref = pathname.replace ("/"+prodDVal+"/", "/"+plat+"/");
+
+			if (pathname.indexOf (homePath) === 0 ) {
+				window.location.replace(newHref);
+            $.cookie (prodKey, plat, {expires: new Date(2020,1,1), path:"/"});
+			}
+
+	}
+
+
+	function modHomeUrls (plt) {
+		$("a[href]").each (function (i) {
+			var $ele = $(this),
+				href = $ele.attr ("href"),
+				parts = href.split("/");
+				fname = parts.pop(),
+				fld = parts.pop(),
+				newHref = href.replace ("/"+prodDVal+"/", "/"+plt+"/"),
+				keeplink = $ele.data ("modlink") === false;
+
+			if (!keeplink && href.indexOf (homePath) === 0 && isValidPattern(href)) {
+				//console.log (href + "=>" + newHref);
+			    $ele.attr("href", newHref);
+			}
+		})
+		
+		// Update product meta value in search form
+		$('#helpSearchForm input[name=product]').attr("value","survey123-" + plat);
+		
+
+	}
+
+	function modContentLinks (plt) {
+		$(".navigation-bar nav a[href], .reference-content a[href], .column-16 a[href]").each (function (i) {
+			var $ele = $(this),
+				href = $ele.attr("href");
+
+				parts = href.split("/");
+				fname = parts.pop(),
+				fld = parts.pop(),
+				newHref = href.replace ("/"+prodDVal+"/", "/"+plt+"/");
+				
+				if(isValidPattern(href)){
+					$ele.attr ("href", newHref);
+				}
+		});
+	}
+	
+
+	if (!isHome) {
+			if(!window.location.pathname.match( /(\/analyze-results\/)/)){
+				$('.reference-content .page-title').after (val);
+			}else{
+				modContentLinks (plat);
+				// Update product meta value in search form
+				$('#helpSearchForm input[name=product]').attr("value","survey123-" + plat);
+			}
+	} else {
+		modHomeUrls (plat);
+		modContentLinks (plat);
+	}
+
+	$("#plats a[data-appname]").each (function (i) {
+		var $ele = $(this),
+			prefix = $ele.data ("prefix"),
+			dplat = $ele.data ("plat"),
+			url;
+
+		if ((fldpath.indexOf (prefix) === 0) )  {
+			$ele.toggleClass ("on");
+			url = prefix + fldpath.replace (prefix, "") + "/" + fname;
+		} else {
+			$ele.toggleClass ("off");
+			url = prefix + "/" + fldpath.split("/").pop() + "/" + fname;
+		}
+
+	
+		$ele.attr ("href", url);
+	});
+
+
+	$('a[data-prefix]').on ("click", function (evt) {
+		var $ele = $(this),
+			url = $ele.attr("href");
+
+		$.cookie ($ele.data ("appname"), $ele.data ("plat"), {expires: new Date(2020,1,1), path:"/"});
+
+		if (isHome) {
+			modHomeUrls ($ele.data ("plat"));
+		}
+	})
+
+});
--- source/js/search/searchcfg.js
+++ source/js/search/searchcfg.js
@@ -112,7 +112,7 @@ var filterCfg = {
         k:"any",
         q:{
             r:"",
-            p:"product:android-app|product:ios-app|product:win-phone-app|product:collector-android|product:collector-ios|product:collector-windows|product:esri-maps-office|product:esri-maps-sharepoint|product:esri-maps-sharepoint-beta|product:ops-dashboard|product:ops-dashboard-browser|product:arcgis-online|product:explorer-ipad|product:explorer-iphone|product:explorer-mac|product:explorer-android-phone|product:explorer-android-tablet|product:esri-demographics|product:arcgis-geoplanner|product:arcgis-open-data|product:web-appbuilder|product:data-appl|product:bus-analyst-online|product:community-analyst|product:esri-maps-cognos|product:esri-maps-cognos-mobile|product:esri-maps-dynamics|product:esri-maps-micro|product:esri-maps-sapbobj|product:esri-maps-salesforce|product:appstudio|product:navigator-iphone|product:navigator-ipad|product:living-atlas|product:arcgis-earth|product:survey123|product:drone2map|product:workforce-iphone|product:workforce-ipad"
+            p:"product:android-app|product:ios-app|product:win-phone-app|product:collector-android|product:collector-ios|product:collector-windows|product:esri-maps-office|product:esri-maps-sharepoint|product:esri-maps-sharepoint-beta|product:ops-dashboard|product:ops-dashboard-browser|product:arcgis-online|product:explorer-ipad|product:explorer-iphone|product:explorer-mac|product:explorer-android-phone|product:explorer-android-tablet|product:esri-demographics|product:arcgis-geoplanner|product:arcgis-open-data|product:web-appbuilder|product:data-appl|product:bus-analyst-online|product:community-analyst|product:esri-maps-cognos|product:esri-maps-cognos-mobile|product:esri-maps-dynamics|product:esri-maps-micro|product:esri-maps-sapbobj|product:esri-maps-salesforce|product:appstudio|product:navigator-iphone|product:navigator-ipad|product:living-atlas|product:arcgis-earth|product:survey123-desktop|product:survey123-browser|product:drone2map|product:workforce-iphone|product:workforce-ipad"
           }
       },
 	  {
@@ -361,9 +361,16 @@ var filterCfg = {
         }
       },
 		{
-        k:"survey123",
+        k:"survey123-desktop",
         q: {
-          r:"(product:survey123)",
+          r:"(product:survey123-desktop)",
+          p:""
+        }
+      },
+		{
+        k:"survey123-browser",
+        q: {
+          r:"(product:survey123-browser)",
           p:""
         }
       },
