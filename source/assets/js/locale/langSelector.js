fba877d49ae1c4dbb5d0bde8e093441d309ff17e
Aimee Broadbent <abroadbent@esri.com>
2017-01-20 16:55:41 -0800

    Remove pt-pt for wab

--- source/assets/js/locale/langSelector.js
+++ source/assets/js/locale/langSelector.js
@@ -440,9 +440,9 @@ jQuery(document).ready(function ($) {
     docCfg.langSelector = "cognos";
   }else if (winloc.pathname.match( /(\/maps-for-microstrategy\/)/)){
     docCfg.langSelector = "micro";
-  }else if (winloc.pathname.match( /(\/marketplace\/|\/collector\/|\/web-appbuilder\/|\/explorer\/|\/appstudio\/|\/operations-dashboard\/|\/workforce\/)/)){
+  }else if (winloc.pathname.match( /(\/marketplace\/|\/collector\/|\/explorer\/|\/appstudio\/|\/operations-dashboard\/|\/workforce\/)/)){
     docCfg.langSelector = "marketplace";
-  }else if (winloc.pathname.match( /(\/workforce\/)/)){
+  }else if (winloc.pathname.match( /(\/workforce\/|\/web-appbuilder\/)/)){
     docCfg.langSelector = "marketplaceptpt";
   }else if (winloc.pathname.match( /(\/navigator\/)/)){
     docCfg.langSelector = "navigator";
