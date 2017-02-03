ce0e76bb4fb0e6efc21773246bd8165140a74ff0
Aimee Broadbent <abroadbent@esri.com>
2017-02-03 08:01:11 -0800

    Fix javascript error in IE

--- source/assets/js/locale/langSelector.js
+++ source/assets/js/locale/langSelector.js
@@ -15,7 +15,8 @@ if (!String.prototype.format) {
     };
 }
 
-function replace_langd(lang='en'){
+function replace_langd(lang){
+  var lang = lang || 'en';
   var help_link = $("a[data-langlabel='help']").attr('href');
   if(help_link.indexOf("%(langd)s") >= 0){
     var new_help_link = help_link.replace("%(langd)s", lang);
